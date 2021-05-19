0427b9fbb7e4526b13824aa9ae2d9bb0d3da18f6
Loïc Bogaert <loicbogaertvauban@gmail.com>
2021-05-19 10:17:21 +0200

    signup et login désormais fonctionnels

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const userRoutes = require('./routes/User');
+
 const db = require('./config/config');
 db.connect();
 
 const app = express();
 
-class App {
+class App{
     constructor(){
-        this.initParser();
-        this.initSetHeaders();
-        this.initSignup();
+        this.initHeaders();
+        this.initRoutes();
     }
-
-    initParser() {
-        app.use(bodyParser.json());
-    }
-
-    initSetHeaders() {
+    initHeaders() {
         app.use((req, res, next) => {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
             next();
           });
-    }
-
-    initSignup() {
-        app.post('/api/auth/signup', (req, res, next) => {
-            const user = new User({
-                email: req.body.email,
-                password: req.body.password
-            });
-            user.save()
-            .then(()=> res.status(201).json({ message: 'utilisateur créé !'}))
-            .catch(error => res.status(400).json ({ error }));
-            })
+          app.use(bodyParser.json());
         };
-    };
 
+        initRoutes(){
+            app.use('/api/auth', userRoutes);
+        };
+};
 
-new App();
 module.exports = app;
+new App();
