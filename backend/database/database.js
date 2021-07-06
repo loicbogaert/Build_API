bcbd16fe1c87f4159edcfccc1c1a9a357c4e648e
Loïc Bogaert <loicbogaertvauban@gmail.com>
2021-07-06 11:21:06 +0200

    last commit

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
- const connect = () => mongoose.connect(process.env.BDD_LOGIN,
+const connect = () => mongoose.connect(process.env.BDD_LOGIN,
     { useNewUrlParser : true,
     useUnifiedTopology : true})
 .then(()=> console.log('Connexion à MongoDB réussie !'))
